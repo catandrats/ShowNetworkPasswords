97e16bf758d823276b72ce0a8aa1382efa716247
GoblinBot <42422577+catandrats@users.noreply.github.com>
2021-10-10 12:27:34 -0500

    Update ShowNetworkPasswords.py

--- Code/ShowNetworkPasswords.py
+++ Code/ShowNetworkPasswords.py
@@ -31,7 +31,7 @@ passin = NONE
 returnlist = [Networks(list), Password(list)]
 '''
 def fetchNetworksandPasswords():
-    data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+    data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles'], stdout=FNULL, stderr=FNULL).decode('utf-8').split('\n')
     networks = []
     for x in data:
         if 'All User Profile' in x:
@@ -43,7 +43,7 @@ def fetchNetworksandPasswords():
     profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
     passwordlist = []
     for i in profiles:
-        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
+        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear'], stdout=FNULL, stderr=FNULL).decode('utf-8').split('\n')
         results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
         for y in results:
             passwordlist.append(str(y))
